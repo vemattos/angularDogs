8ead1a48f303a97be22ff1d685f14ffba0ac4c31
Vinícius Estrázulas Mattos <vinicius.emattos@gmail.com>
2024-04-18 16:13:00 -0300

    checkbox

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,72 +4,86 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-
+export class LoginComponent implements OnInit {
   usuario: string | undefined;
   senha: string | undefined;
 
-  constructor(private router: Router){
-
-  }
+  constructor(private router: Router) {}
 
   onSubmit() {
-    let nomeUsuario = (<HTMLInputElement>document.getElementById("username")).value;
-    let senha = (<HTMLInputElement>document.getElementById("password")).value;
+    let nomeUsuario = (<HTMLInputElement>document.getElementById('username'))
+      .value;
+    let senha = (<HTMLInputElement>document.getElementById('password')).value;
 
     if (nomeUsuario.length < 3) {
-      alert("O nome de usuário deve conter pelo menos 3 caracteres");
+      alert('O nome de usuário deve conter pelo menos 3 caracteres');
       return;
     }
 
     if (senha.length < 3) {
-      alert("A senha deve conter pelo menos 3 caracteres");
+      alert('A senha deve conter pelo menos 3 caracteres');
       return;
     }
 
-    if (nomeUsuario === "adm" && senha === "123") {
+    if (nomeUsuario === 'adm' && senha === '123') {
       this.router.navigate(['/dogs']);
     } else {
-      alert("Credenciais inválidas. Por favor, tente novamente.");
+      alert('Credenciais inválidas. Por favor, tente novamente.');
     }
-}
-
-  forgotPassword(){
-    let nomeUsuario = (<HTMLInputElement>document.getElementById("username")).value;
-    let senha = (<HTMLInputElement>document.getElementById("password")).value;
-
-    alert(nomeUsuario + " sua senha é: " + senha);
   }
 
-  cadastrar(){
-    alert("Usuário cadastrado! -> Nome de usuário: adm | Senha: 123");
+  forgotPassword() {
+    let nomeUsuario = (<HTMLInputElement>document.getElementById('username'))
+      .value;
+    let senha = (<HTMLInputElement>document.getElementById('password')).value;
+
+    alert(nomeUsuario + ' sua senha é: ' + senha);
   }
 
-//   onChecked() {
-//     let rememberMe = (<HTMLInputElement>document.getElementById("remember")).checked;
-//     if (rememberMe) {
-//         let nomeUsuario = (<HTMLInputElement>document.getElementById("username")).value;
-//         let senha = (<HTMLInputElement>document.getElementById("password")).value;
-//         sessionStorage.setItem("username", nomeUsuario);
-//         sessionStorage.setItem("password", senha);
-//     } else {
-//         sessionStorage.removeItem("username");
-//         sessionStorage.removeItem("password");
-//     }
-// }
+  cadastrar() {
+    alert('Usuário cadastrado! -> Nome de usuário: adm | Senha: 123');
+  }
 
-ngOnInit() {
-//     let usernameSession = sessionStorage.getItem("username");
-//     let passwordSession = sessionStorage.getItem("password");
-//     if (usernameSession !== null && passwordSession !== null) {
-//         (<HTMLInputElement>document.getElementById("username")).value = usernameSession;
-//         (<HTMLInputElement>document.getElementById("password")).value = passwordSession;
-//     }
-}
+  onChecked() {
+    let rememberMe = (<HTMLInputElement>document.getElementById('remember'))
+      .checked;
+    let check = sessionStorage.getItem('checked');
+    console.log(check);
+    let usernameSession = sessionStorage.getItem('username');
+    let passwordSession = sessionStorage.getItem('password');
+    console.log(usernameSession);
+    console.log(passwordSession);
+    if (
+      usernameSession !== null &&
+      passwordSession !== null &&
+      rememberMe &&
+      check == 'true'
+    ) {
+      (<HTMLInputElement>document.getElementById('username')).value =
+        usernameSession;
+      (<HTMLInputElement>document.getElementById('password')).value =
+        passwordSession;
+    }
+    sessionStorage.setItem('checked', rememberMe ? 'true' : 'false');
+    if (check) {
+      let nomeUsuario = (<HTMLInputElement>document.getElementById('username'))
+        .value;
+      let senha = (<HTMLInputElement>document.getElementById('password')).value;
+      sessionStorage.setItem('username', nomeUsuario);
+      sessionStorage.setItem('password', senha);
+    } else {
+      sessionStorage.removeItem('username');
+      sessionStorage.removeItem('password');
+    }
+    if (usernameSession !== null && passwordSession !== null && check == "true") {
+      (<HTMLInputElement>document.getElementById('username')).value =
+        usernameSession;
+      (<HTMLInputElement>document.getElementById('password')).value =
+        passwordSession;
+    }
+  }
 
-
-
-
+  ngOnInit() {this.onChecked()}
 }
